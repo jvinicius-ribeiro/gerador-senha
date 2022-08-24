d60c33a158dab7b2f1d963562fbb0987c3142c90
Vinicius Ribeiro <jvinicius.ribeiro@proton.me>
2022-08-24 19:01:39 -0300

    Adicionando arquivos do projeto já finalizado

---
+++ script.js
@@ -0,0 +1,36 @@
+// REVISAR CODIGO, slider não está funcionando tão bem como deveria. 
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector ("#senha");
+
+let containerPassword = document.querySelector ("#painel-senha");
+
+let charset = "abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRTUVXWYZ0123456789!@#$%¨&*()-_+=";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.onInput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    
+    let pass = "";
+
+    for( let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() *n));
+    }
+
+    containerPassword.classList.remove("hide");    
+    password.innerHTML = pass;
+    novaSenha= pass;
+}
+
+function copyPassword(){
+    alert("Sua senha foi copiada!");
+    navigator.clipboard.writeText(novaSenha);
+}
